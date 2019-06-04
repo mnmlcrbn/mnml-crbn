0c2bb897e73d723cb704df833aed468da336fcdc
David Tonge <contact@davidtonge.me>
2019-06-03 21:14:35 -0400

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Minimal Caribbean`,
+    description: `Full Service Creative Agency based in St.Croix U.S.V.I focused on changing the digital landscape of the caribbean.`,
     author: `@gatsbyjs`,
   },
   plugins: [
